a92e4b34c6080d5b2f1b80653008c8007418b9c8
Sho2134 <136536205+Sho2134@users.noreply.github.com>
2023-06-14 09:22:35 +0900

    demo site init

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+{
+    alert('注意　これはデモサイトです。')
+    const targets = document.querySelectorAll('.target');
+
+    function callback(entries,obs) {
+
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+               return;
+            }
+            entry.target.classList.add('appear');
+            obs.unobserve(entry.target);
+        });
+    }
+
+
+    const options = {
+        threshold: 0.5,
+    };
+    const observer = new IntersectionObserver(callback,options);
+
+    targets.forEach(target => {
+        observer.observe(target);
+    })
+
+    
+}
